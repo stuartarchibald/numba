6c2f5c8fdc13b1c519685c342fac0da71062f138
Ehsan Totoni <ehsan.totoni@intel.com>
2017-08-16 08:01:51 -0700

    support np.iinfo() min/max

--- numba/typing/arraydecl.py
+++ numba/typing/arraydecl.py
@@ -473,6 +473,15 @@ class FInfoAttr(AttributeTemplate):
     def resolve_min(self, ary):
         return ary.dtype.dtype
 
+@infer_getattr
+class IInfoAttr(AttributeTemplate):
+    key = types.IInfo
+
+    def resolve_max(self, ary):
+        return ary.dtype.dtype
+
+    def resolve_min(self, ary):
+        return ary.dtype.dtype
 
 @infer
 class StaticGetItemArray(AbstractTemplate):
