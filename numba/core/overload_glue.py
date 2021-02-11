6bff85d994abae61c3b6445688cd6a63fd0a8f5b
Stuart Archibald <stuart.archibald@googlemail.com>
2021-02-11 13:14:50 +0000

    hack around issues with StarArgTuple using function generation
    as title

--- numba/core/overload_glue.py
+++ numba/core/overload_glue.py
@@ -1,5 +1,43 @@
 import numpy as np
 
+import types as pytypes
+
+
+def stub_generator(nargs, glbls):
+    def stub(tyctx):
+        return body(tyctx)
+    # create new code parts
+    stub_code = stub.__code__
+    co_args = [stub_code.co_argcount + nargs]
+
+    new_varnames = [*stub_code.co_varnames]
+    new_varnames.extend([f'tmp{x}' for x in range(nargs)])
+    from numba.core import utils
+    if utils.PYVERSION >= (3, 8):
+        co_args.append(stub_code.co_posonlyargcount)
+    co_args.append(stub_code.co_kwonlyargcount)
+    co_args.extend([stub_code.co_nlocals + nargs,
+                    stub_code.co_stacksize,
+                    stub_code.co_flags,
+                    stub_code.co_code,
+                    stub_code.co_consts,
+                    stub_code.co_names,
+                    tuple(new_varnames),
+                    stub_code.co_filename,
+                    stub_code.co_name,
+                    stub_code.co_firstlineno,
+                    stub_code.co_lnotab,
+                    stub_code.co_freevars,
+                    stub_code.co_cellvars
+                    ])
+
+    new_code = pytypes.CodeType(*co_args)
+
+    # get function
+    new_func = pytypes.FunctionType(new_code, glbls)
+    return new_func
+
+
 class OverloadWrapper(object):
 
     def __init__(self, function=None):
@@ -19,6 +57,7 @@ class OverloadWrapper(object):
         """
         assert concrete_function is self._function, "Wrong typing wrapper"
         # concrete_function is e.g. the numpy function this is implementing
+
         def inner(typing_class):
             # arg is the typing class
             self._TYPER = typing_class
@@ -35,6 +74,7 @@ class OverloadWrapper(object):
         # args is (concrete_function, *numba types)
         concrete_function = args[0]
         assert concrete_function is self._function, "Wrong impl wrapper"
+
         def inner(lowerer):
             self._BIND_TYPES[args[1:]] = lowerer
             return lowerer
@@ -53,16 +93,16 @@ class OverloadWrapper(object):
 
     def _build(self):
         from numba.core.extending import overload, intrinsic
+        from numba import types
 
         @overload(self._function)
-        def ol_generated(*args):
-            @intrinsic
-            def intrin(tyctx, *intrin_args):
+        def ol_generated(*ol_args):
+
+            def body(tyctx):
                 msg = f"No typer registered for {self._function}"
                 assert self._TYPER is not None, msg
                 typing = self._TYPER(tyctx)
-                sig = typing.apply(intrin_args, {})
-                print(sig)
+                sig = typing.apply(ol_args, {})
                 if self._selector is None:
                     self._assemble()
                 lowering = self._selector.find(sig.args)
@@ -71,8 +111,11 @@ class OverloadWrapper(object):
                 assert lowering is not None, msg
                 return sig, lowering
 
-            def jit_wrapper(*args):
-                return intrin(*args)
+            stub = stub_generator(len(ol_args), {'body': body})
+            intrin = intrinsic(stub)
+
+            def jit_wrapper(*ol_args):
+                return intrin(*ol_args)
             return jit_wrapper
 
 
@@ -91,4 +134,3 @@ class Gluer():
 
 overload_glue = Gluer()
 del Gluer
-
